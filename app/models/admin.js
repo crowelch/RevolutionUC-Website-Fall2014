35f48e63cd85b405d4a5661cee1db2d275a56add
Chris Crowell <crowelch@mail.uc.edu>
2015-03-17 18:50:16 -0400

    hotfix!

---
+++ app/models/admin.js
@@ -0,0 +1,6 @@
+var mongoose = require('mongoose');
+var registrationDateSchema = mongoose.Schema({
+	
+});
+
+module.export(registrationDateSchema);
--- public/js/app.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('RevUc', ['ui.bootstrap'])
-    .controller('Files', function($scope, $http, $rootScope) {
-
-    })
-    .directive('fileUpload', function () {
-
-    });
